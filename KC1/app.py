c92a29cc8807df2898cc2bbf7b131ff44fbf6014
Thane Chaves <tmoonder@gmail.com>
2025-01-18 15:57:48 -0500

    Successfully connected database to app.

--- KC1/app.py
+++ KC1/app.py
@@ -16,23 +16,44 @@ import requests
 import sqlite3
 from datetime import datetime
 
+
+app = Flask(__name__)
+
 # Write a script to scrape news articles from a website (e.g., CNN, BBC)
 def get_news():
     r = requests.get("https://www.wtvr.com/")
     soup = BeautifulSoup(r.content, "html.parser")
     headlines = [x.get_text() for x in soup.find_all("h3", class_="ListItem-title")]
-    # print(headlines)
     return headlines
 
-
-app = Flask(__name__)
+def create_database():
+    headlines = get_news()
+    conn = sqlite3.connect('news.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS news (id INTEGER PRIMARY KEY, headline TEXT)''')
+    print("created news table")
+    for headline in headlines:
+        c.execute('INSERT INTO news (headline) VALUES (?)', (headline,))
+    print("added headlines to news table")
+    conn.commit()
+    conn.close()
 
 
 @app.route("/")
 def main():
-    headlines = get_news()
-    html = ["<p>{text}</p>".format(text=x) for x in headlines]
-    return "<p>Here is the content.</p><div>{html}</div>".format(html="".join(html))
+    conn = sqlite3.connect('news.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM news')
+    news = c.fetchall()
+    conn.close()
+    print("running")
+    # news = "hi"
+    return "<p>{news}</p>".format(news=news)
+    # headlines = get_news()
+    # html = ["<p>{text}</p>".format(text=x) for x in headlines]
+    # return "<p>Here is the content.</p><div>{html}</div>".format(html="".join(html))
 
 
-get_news()
+if __name__ == '__main__':
+    create_database()
+    app.run(debug=True)
